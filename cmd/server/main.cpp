ef8a5cfed97f9f11a0593a112cb9c46420078b5d
Rashi <bandit@overthewire.org>
2026-07-03 02:48:16 +0530

    Added MinIO client and file upload support

--- cmd/server/main.cpp
+++ cmd/server/main.cpp
@@ -7,6 +7,9 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <pqxx/pqxx> // PostgreSQL C++ connector
+#include <nlohmann/json.hpp>
+#include "../../internal/storage/minio_client.hpp"
+using json = nlohmann::json;
 
 // Maximum concurrent connections allowed
 const int MAX_CONNECTIONS = 10;
@@ -42,6 +45,96 @@ void initialize_database(){
 		std::cerr << "[Database Error]" << e.what() << "\n";
 		exit(1);
 	}
+}
+
+void handle_client(int client_fd) {
+	char buffer[1024] = {0};
+            
+            // Read incoming HTTP request
+            int bytes_read = read(client_fd, buffer, sizeof(buffer) - 1);
+            if (bytes_read > 0) {
+                std::string request(buffer);
+
+		if(request.find("POST /register") != std::string::npos) {
+		size_t body_pos = request.find("\r\n\r\n");
+		if(body_pos != std::string::npos) {
+		std::string body = request.substr(body_pos +4);
+
+		try {
+		   json req_json = json::parse(body);
+		   std::string user = req_json["username"];
+		   std::string pass = req_json["password"]; // this is what is needed to be hashed
+		   const char* dp_pass = std::getenv("DB_PASSWORD");
+		   std::string conn_str = "host=127.0.0.1 port=5432 user=vault_admin password="+std::string(dp_pass)+" dbname=obscura_vault";
+		   pqxx::connection conn{conn_str};
+
+		   pqxx::work tx{conn};
+		   tx.exec(
+				   "INSERT INTO users (username, password_hash) VALUES ($1, $2)",
+				  pqxx::params{user, pass}
+				 );
+		   tx.commit();
+
+		std::cout << "[Server] Registration Recieved Data: " << body << "\n";                
+		std::string response = "HTTP/1.1 200 OK\r\n"
+		                      "Content-Length: 10\r\n"
+				      "\r\n"
+				      "Registered";
+
+		send(client_fd, response.c_str(), response.length(), 0);
+		}
+		catch (const std::exception& e) {
+			std::cerr << "[Registration Error] " << e.what() << "\n";
+			std::string err_response = "HTTP/1.1 400 Bad Request\r\n"
+				                  "Content-Length: 15\r\n"
+						  "\r\n"
+						  "Request Failed\n";
+			send(client_fd, err_response.c_str(), err_response.length(), 0);
+		}
+            }
+	}
+
+	   else if(request.find("POST /upload") != std::string::npos) {
+		   size_t body_pos = request.find("\r\n\r\n");
+		   if(body_pos != std::string::npos) {
+			   std::string file_content = request.substr(body_pos + 4);
+
+			   MinioClient storage("http://localhost:9000", "minioadmin", "minioadmin", "obscura-api");
+  
+			   std::string response;
+			   if(storage.upload_file("user_upload.bin", file_content)){
+				   response = "HTTP/1.1 200 OK\r\n"
+					                  "Content-length: 15\r\n"
+							  "\r\n"
+							  "Upload Success";
+
+			   }
+			   else {
+				response = "HTTP/1.1 500 Internal Server Error\r\n"
+                                           "Content-Length: 13\r\n"
+                                           "\r\n"
+                                           "Upload Failed";
+			   }
+			   send(client_fd, response.c_str(), response.length(), 0);
+		   }
+	   }
+
+	   else if(request.find("GET /") != std::string::npos) {
+	   std::string response = "HTTP/1.1 200 OK\r\n"
+	                          "Content-Length: 22\r\n"
+				  "\r\n"
+				  "Welcome to the Vault.";
+
+            // Send response back
+            send(client_fd, response.c_str(), response.length(), 0);
+	   }
+		}
+
+            // Close connection
+            close(client_fd);
+
+            // Decrement the atomic counter
+            active_conn--;
 }
 
 int main() {
@@ -109,47 +202,9 @@ int main() {
         // We pass an anonymous lambda block directly into std::thread.
         // [client_fd] is captured by value so every thread gets its own socket copy.
         // [&active_conn] is captured by reference so we can safely decrement it when finished.
-        std::thread([client_fd]() {
-            char buffer[1024] = {0};
-            
-            // Read incoming HTTP request
-            int bytes_read = read(client_fd, buffer, sizeof(buffer) - 1);
-            if (bytes_read > 0) {
-                std::string request(buffer);
-
-		if(request.find("POST /register") != std::string::npos) {
-		size_t body_pos = request.find("\r\n\r\n");
-		if(body_pos != std::string::npos) {
-		std::string body = request.substr(body_pos +4);
-
-		std::cout << "[Server] Recieved Registration Data: " << body << "\n";                
-		std::string response = "HTTP/1.1 200 OK\r\n"
-		                       "Content-Length: 10\r\n"
-				       "\r\n"
-				       "Registered";
-
-		send(client_fd, response.c_str(), response.length(), 0);
-
-            }
-	    }
-	    else if(request.find("GET /") != std::string::npos) {
-	    std::string response = "HTTP/1.1 200 OK\r\n"
-	                           "Content-Length: 22\r\n"
-				   "\r\n"
-				   "Welcome to the Vault.";
-
-            // Send response back
-            send(client_fd, response.c_str(), response.length(), 0);
-	    }
-		}
-
-            // Close connection
-            close(client_fd);
-
-            // Decrement the atomic counter
-            active_conn--;        }).detach(); // Detach allows thread resources to auto-cleanup upon finishing
+        std::thread(handle_client, client_fd).detach();
     }
-
+            
     close(server_fd);
     return 0;
 }
---
+++ internal/storage/minio_client.hpp
@@ -0,0 +1,58 @@
+#pragma once
+#include <string>
+#include <iostream>
+#include <curl/curl.h>
+
+class MinioClient {
+private:
+	std::string endpoint;
+	std::string access_key;
+	std::string secret_key;
+	std::string bucket_name;
+
+public:
+	MinioClient(std::string ep, std::string ak, std::string sk, std::string bucket)
+        : endpoint(ep), access_key(ak), secret_key(sk), bucket_name(bucket) 
+    {
+
+        std::cout << "[Storage] Client initialized for: " << endpoint << "/" << bucket_name << std::endl;
+    }
+
+	bool upload_file(const std::string& filename, const std::string& file_data){
+		if(endpoint.empty()) {
+			std::cerr << "[Storage] Error: Endpoint is empty!" << std::endl;
+            return false;
+		}
+		CURL* curl = curl_easy_init();
+		if(!curl) 
+			return false;
+
+		char errbuf[CURL_ERROR_SIZE];
+		curl_easy_setopt(curl, CURLOPT_ERRORBUFFER, errbuf);
+
+		std::string url = endpoint + "/" + bucket_name + "/" + filename;
+
+		struct curl_slist* headers = nullptr;
+		headers = curl_slist_append(headers, "Content-Type: application/octet-stream");
+
+		curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+		curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "PUT");
+		curl_easy_setopt(curl, CURLOPT_POSTFIELDS, file_data.c_str());
+		curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, (long)file_data.size());
+		curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
+		curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+
+		CURLcode res = curl_easy_perform(curl);
+
+		long http_code = 0;
+		curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
+                if(res != CURLE_OK) {
+			std::cerr << "[libcurl] Error: " << errbuf << std::endl;
+		}
+		std::cout << "[Storage] HTTP Status Code: " << http_code << std::endl;
+		curl_slist_free_all(headers);
+		curl_easy_cleanup(curl);
+
+		return (res == CURLE_OK && http_code == 200);
+	}
+};
